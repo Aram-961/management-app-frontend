17a2c83d3c837fed011142247985e3073a783f93
Aram-961 <aramistamboulian961@gmail.com>
2024-01-04 11:25:37 +0200

    implementing the delete functionality and solved a minor bug

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@ import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Client from "./components/Client";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          keyArgs: false,
+          merge: (existing, incoming) => {
+            return incoming;
+          },
+        },
+
+        projects: {
+          keyArgs: false,
+          merge: (existing, incoming) => {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:8000/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 function App() {
   return (
--- src/components/ClientRow.jsx
+++ src/components/ClientRow.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import { CiTrash } from "react-icons/ci";
+import { useMutation } from "@apollo/client";
+import { DELETE_CLIENT } from "../mutations/clientMutation.js";
+import { GET_CLIENTS } from "../queries/clientQuery.js";
 
 const ClientRow = ({ client }) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    // On Delete: Refetch client from database
+    // refetchQueries: [{ query: GET_CLIENTS }],
+
+    // updating the cache on deleteClient
+    update: (cache, { data: { deleteClient } }) => {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: clients.filter((c) => c.id !== deleteClient.id) },
+      });
+    },
+  });
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className='btn btn-alert'>
+        <button className='btn btn-alert' onClick={deleteClient}>
           <CiTrash />
         </button>
       </td>
