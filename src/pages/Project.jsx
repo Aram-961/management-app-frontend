0193bdd91d5541f25c68844cebd3011c66df8b5f
Aram-961 <aramistamboulian961@gmail.com>
2024-01-07 10:21:05 +0200

    fixed the routing project/:id bug

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -11,9 +11,17 @@ const Project = () => {
 
   if (loading) return <div>Loading...</div>;
   if (error) return <h1>Something went wrong...</h1>;
-  return <div>
-    
-  </div>;
+  return (
+    <>
+      {!loading && !error && (
+        <div className='mx-auto w-75 card p-5'>
+          <Link to='/' className='btn btn-light btn-sm w-25 d-inline ms-auto'>
+            back
+          </Link>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Project;
